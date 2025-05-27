2ab259f2582a7aa5deb7d218a77ce82834ff338f
yudai1025z <yudai1025zzz@icloud.com>
2025-05-28 01:41:19 +0900

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE SedapMakanDB;
 GO
 
+USE SedapMakanDB;
+GO
 CREATE TABLE Users 
 (
     userName varchar(50) NOT NULL PRIMARY KEY,
 	userPassword VARCHAR(20) NOT NULL,
     userRole varchar(50) NOT NULL
 );
-
-create database menudata;
 
 CREATE TABLE Menu(
 
