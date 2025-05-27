2c83d7a2303f6af6196f4ffb1daedf98965a0a17
VOID131105 <voidbeingvoid@gmail.com>
2025-05-28 00:39:48 +0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -4,8 +4,8 @@ GO
 CREATE TABLE Users 
 (
     userName varchar(50) NOT NULL PRIMARY KEY,
-	userPassword VARCHAR(20),
-    userRole varchar(50)
+	userPassword VARCHAR(20) NOT NULL,
+    userRole varchar(50) NOT NULL
 );
 
 create database menudata;
